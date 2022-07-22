5b3ac65a0309e9682c1ca709c2ea4355563053cc
DragonEggBedrockBreaking <antonbhaumik@proton.me>
2022-07-22 20:47:45 +0100

    Bruh

--- buildscript/src/main/java/Versions.java
+++ buildscript/src/main/java/Versions.java
@@ -1,6 +1,6 @@
 public class Versions {
     public static String MINECRAFT_VERSION = "1.19";
-    public static String QUILT_LOADER_VERSION = "0.17.0-beta.6";
+    public static String QUILT_LOADER_VERSION = "0.17.1-beta.6";
     public static int JAVA_VERSION = 17;
     public static String QUILTFLOWER_VERSION = "1.8.1";
     public static String CARPET_VERSION = "1.19-1.4.79+v220607";
