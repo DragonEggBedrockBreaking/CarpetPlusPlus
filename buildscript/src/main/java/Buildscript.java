43810312850c62d03638e5c3d998b1de74500e79
DragonEggBedrockBreaking <antonbhaumik@proton.me>
2022-07-22 20:46:11 +0100

    Modernise Buildscript

--- buildscript/src/main/java/Buildscript.java
+++ buildscript/src/main/java/Buildscript.java
@@ -8,7 +8,6 @@ import io.github.coolcrabs.brachyura.maven.Maven;
 import io.github.coolcrabs.brachyura.maven.MavenId;
 import io.github.coolcrabs.brachyura.minecraft.Minecraft;
 import io.github.coolcrabs.brachyura.minecraft.VersionMeta;
-import io.github.coolcrabs.brachyura.processing.ProcessorChain;
 import io.github.coolcrabs.brachyura.quilt.QuiltMaven;
 import io.github.coolcrabs.brachyura.quilt.SimpleQuiltProject;
 import net.fabricmc.mappingio.tree.MappingTree;
@@ -16,32 +15,17 @@ import net.fabricmc.mappingio.tree.MappingTree;
 public class Buildscript extends SimpleQuiltProject {
     @Override
     public VersionMeta createMcVersion() {
-        // Minecraft Version
         return Minecraft.getVersion(Versions.MINECRAFT_VERSION);
     }
 
     @Override
     public MappingTree createMappings() {
-        // Uses Mojang Official Mappings
         return createMojmap();
     }
 
     @Override
     public FabricLoader getLoader() {
-        // Quilt Loader Version
         return new FabricLoader(QuiltMaven.URL, QuiltMaven.loader(Versions.QUILT_LOADER_VERSION));
-    }
-
-    @Override
-    public String getModId() {
-        // Mod Name
-        return "carpetplusplus";
-    }
-
-    @Override
-    public String getVersion() {
-        // Mod Version
-        return Versions.MOD_VERSION;
     }
 
     @Override
@@ -54,25 +38,16 @@ public class Buildscript extends SimpleQuiltProject {
 
     @Override
     public int getJavaVersion() {
-        // Default is Java 8
         return Versions.JAVA_VERSION;
     }
 
     @Override
     public BrachyuraDecompiler decompiler() {
-        // Uses QuiltFlower instead of CFR
         return new FernflowerDecompiler(Maven.getMavenJarDep(QuiltMaven.URL, new MavenId("org.quiltmc", "quiltflower", Versions.QUILTFLOWER_VERSION)));
     };
 
     @Override
     public Path getBuildJarPath() {
-        // Changes the jar file name
         return getBuildLibsDir().resolve(getModId() + "-" + "mc" + createMcVersion().version + "-" + getVersion() + "-quilt" + ".jar");
-    }
-
-    @Override
-    public ProcessorChain resourcesProcessingChain() {
-        // Patches version in quilt.mod.json
-        return new ProcessorChain(super.resourcesProcessingChain(), new QmjVersionPatcher());
     }
 }
