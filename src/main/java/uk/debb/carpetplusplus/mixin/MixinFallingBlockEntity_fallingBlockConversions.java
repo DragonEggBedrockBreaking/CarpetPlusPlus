7e13cb75b809435a3f22a3e64b9cee784dcae078
DragonEggBedrockBreaking <antonbhaumik@proton.me>
2022-08-14 16:34:34 +0100

    Discard falling blocks after 2gt to make it more difficult

--- src/main/java/uk/debb/carpetplusplus/mixin/MixinFallingBlockEntity_fallingBlockConversions.java
+++ src/main/java/uk/debb/carpetplusplus/mixin/MixinFallingBlockEntity_fallingBlockConversions.java
@@ -10,8 +10,15 @@ import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import uk.debb.carpetplusplus.CarpetPlusPlusSettings;
 import uk.debb.carpetplusplus.anvil.AnvilStaticFields;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 @Mixin(FallingBlockEntity.class)
 public abstract class MixinFallingBlockEntity_fallingBlockConversions extends Entity {
@@ -44,6 +51,29 @@ public abstract class MixinFallingBlockEntity_fallingBlockConversions extends En
                     }
                 }
             }
+        }
+    }
+
+    /**
+     * @param ci the callback info
+     * @author DragonEggBedrockBreaking
+     * @reason schedule the block's deletion after 2gt
+     */
+    @Inject(method = "tick", at = @At("HEAD"))
+    private void scheduleDeletion(CallbackInfo ci) {
+        // Check for carpet rule and check if it is one of the falling blocks
+        if (CarpetPlusPlusSettings.fallingBlockConversions && AnvilStaticFields.unobtainableBlockList.contains(this.blockState.getBlock())) {
+            // Create a falling block entity instance
+            FallingBlockEntity fallingBlockEntity = (FallingBlockEntity) (Object) this;
+            // Schedule deletion in 2gt
+            ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+            Runnable task = () -> {
+                if (fallingBlockEntity != null) {
+                    fallingBlockEntity.discard();
+                }
+            };
+            executorService.schedule(task, 100, TimeUnit.MILLISECONDS);
+            executorService.shutdown();
         }
     }
 }
